9647384468437a282d549d706b9afec435fa609e
Inderpreet97 <inderpreet.singh1@studenti.unipr.it>
2019-09-17 17:12:40 +0200

    Update ClienteProgram.cs

--- Progetto/WCFClient/WCFClient/ClienteProgram.cs
+++ Progetto/WCFClient/WCFClient/ClienteProgram.cs
@@ -8,6 +8,25 @@ namespace WCFClient
         public ClienteProgram()
             : base("Menu Cliente", breadcrumbHeader: true)
         {
+
+            /* : base(string title, bool breadcrumbHeader)
+             * 
+             * Chiamata al costruttore della classe base, cioè il costruttore della classe Program.
+             *   - string title: titolo della homepage
+             *   - bool breadcrumbHeader: attiva/disattiva visualizzazione della cronologia delle pagine aperte
+             * 
+             * La classe Program è una classe Abstract, quindi non possiamo istanzare oggetti di Program,
+             * dobbiamo prima creare una classe figlia di Program.
+             * 
+             * La classe program contiene una Dictionary <type, Page> che contiene tutte le pagine che verrano usate
+             * in questo programma. AddPage non fa altro che aggiungere una Page a questo dictionary.
+             * 
+             * Program contiene anche una proprietà chiamata History di tipo Stack<Page> che permette di mantenere
+             * creare una cronologia delle Page aperte.
+             * 
+             * 
+             * 
+             */
             AddPage(new MainMenuCliente(this));
             AddPage(new ListaMovimenti(this));
             AddPage(new MovimentiMenu(this));
