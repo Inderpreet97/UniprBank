9f00e1f51c2b136c4ced78b5930953c58c4e7708
GiuseppeUrbano <51412290+GiuseppeUrbano@users.noreply.github.com>
2019-09-29 12:09:58 +0200

    Commit

--- Progetto/WCFClient/WCFClient/MainProgram.cs
+++ Progetto/WCFClient/WCFClient/MainProgram.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using EasyConsole;
 using System.Configuration;
 using WCFClient.ServiceReference1;
+using EasyConsole;
 using ConsoleTables;
+
 
 namespace WCFClient
 {
